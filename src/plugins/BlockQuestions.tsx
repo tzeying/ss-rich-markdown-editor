de64d6d69b497e5d6174512571961dbcf47f3412
Tzeying <tzeyingcheng@gmail.com>
2021-07-24 22:31:12 +0800

    add plugins

--- src/plugins/BlockQuestions.tsx
+++ src/plugins/BlockQuestions.tsx
@@ -13,14 +13,13 @@ export default class Keys extends Extension {
             new Plugin({
                 props: {
                     handleKeyDown: (view, event) => {
-                        console.log("A key was pressed!")
                         const serializer = this.editor.serializer
                         let n = view.state.doc;
                         let content = n.content['content']
                         for (const node in content) {
                             console.log(content[node].type.name)
+                            console.log(serializer.serialize(content[node]))
                             if (content[node].type.name == "question") {      
-                                console.log(serializer.serialize(content[node]))
                             }
                         }
                         return false // We did not handle this
